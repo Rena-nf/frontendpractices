55d19788aa7c5fbef190612767c57feba22642b6
Rena-nf <thenamealreadytakentwice@gmail.com>
2025-02-04 22:33:10 +0700

    unfinished navbar

--- ableton/src/components/navbar.tsx
+++ ableton/src/components/navbar.tsx
@@ -1,9 +1,49 @@
 import logo from "@/assets/logo.svg";
 
+const FullNav = () => {
+    return (
+        <div className="navlist">
+            <a href={"#"}>Live</a>
+            <a href={"#"}>Push</a>
+            <a href={"#"}>Move</a>
+            <a href={"#"}>Note</a>
+            <a href={"#"}>Link</a>
+            <a href={"#"}>Shop</a>
+            <a href={"#"}>Packs</a>
+            <a href={"#"}>Help</a>
+            <a href={"#"}>More+</a>
+        </div>
+    );
+};
+
+const SmallNav = () => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+};
+
 export default function NavBar() {
-  return (
-    <div>
-      <img className="" src={logo} alt={"Site logo"} />
-    </div>
-  );
+    let NavButton;
+
+    if (window.innerWidth <= 1024) {
+        NavButton = <SmallNav />;
+    } else {
+        NavButton = <FullNav />;
+    }
+
+    return (
+        <div className=".navbar">
+            <div>
+                <a className="logo_container" href="#">
+                    <img className="logo" src={logo} alt={"Site logo"} />
+                </a>
+                {NavButton}
+            </div>
+            <div></div>
+            <div>
+            </div>
+        </div>
+    );
 }
